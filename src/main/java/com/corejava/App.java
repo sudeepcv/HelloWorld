ea77a4d1f451a39268b5dfdad1fed9c46dd81365
Sudeep-cv <basicsinjava@gmail.com>
2019-05-29 09:57:13 +0000

    Collections.sor using lambda

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -1,13 +1,46 @@
 package com.corejava;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        List<Person> people = Arrays.asList(new Person("sudeep", 29), new Person("manu", 30), new Person("Tinu", 24));
+
+        System.out.println("before sort:");
+
+        PrintAllPeople(people);
+
+        // java 7 sort
+
+        // Collections.sort(people, new Comparator<Person>() {
+        //     @Override
+        //     public int compare(Person o1, Person o2) {
+        //         return o1.getName().compareTo(o2.getName());
+        //     }
+        // });
+
+        System.out.println("after sort:");
+
+        // PrintAllPeople(people);
+
+        // java 8 lambda sort
+
+        Collections.sort(people,(Person o1,Person o2)->{ return o1.getName().compareTo(o2.getName());});
+        PrintAllPeople(people);
+
+    }
+
+    private static void PrintAllPeople(List<Person> people) {
+        for (Person p : people) {
+            System.out.println(p.getName());
+        }
     }
 }
---
+++ src/main/java/com/corejava/Person.java
@@ -0,0 +1,27 @@
+package com.corejava;
+
+public class Person{
+
+    private String name;
+    private int age;
+
+    Person(String name,int age){
+        super();
+        this.setName(name);
+        this.setAge(age);
+    }
+
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
