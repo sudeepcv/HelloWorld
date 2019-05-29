a02d9d1c656ef74182f862b74cf16e374289cb84
Sudeep-cv <basicsinjava@gmail.com>
2019-05-29 07:28:12 +0000

    runnable using lambda

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -7,7 +7,18 @@ package com.corejava;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        Thread thread=new Thread(new Runnable(){
+        
+            @Override
+            public void run() {
+                System.out.println("Hello runnable");
+            }
+        });
+        thread.run();
+
+
+        Thread lambdaThread=new Thread(() -> {System.out.println("Hello runnable Lambda!!");});
+        lambdaThread.run();
     }
 }
