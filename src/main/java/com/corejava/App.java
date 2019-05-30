68302cbb95d69bb62dbdde1f48122fee807df544
Sudeep-cv <basicsinjava@gmail.com>
2019-05-30 04:10:56 +0000

    method inside interface

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -4,10 +4,20 @@ package com.corejava;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Java8Class java8Class = new Java8Class();
+        java8Class.print();
+
     }
+}
+
+interface Java8Interface {
+    default void print() {
+        System.out.println("test");
+    }
+}
+
+class Java8Class implements Java8Interface {
+
 }
