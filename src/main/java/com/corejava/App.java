37d2e86dccbb9692948fbc6d4987dea33f4d0aca
Sudeep-cv <basicsinjava@gmail.com>
2019-05-31 04:02:09 +0000

    comparable

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -1,10 +1,9 @@
 package com.corejava;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Hello world!
@@ -12,33 +11,13 @@ import java.util.Random;
  */
 public class App {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        Random r=new Random();
+        List<Person> people = Arrays.asList(new Person("sudeep", 29), new Person("manu", 30), new Person("Tinu", 24));
+        people.forEach(p -> System.out.println(p.toString()));
+        Collections.sort(people);
+        System.out.println("after sort:");
 
-
-        for(int i=0;i<100;i++){
-            list.add( r.nextInt(100));
-        }
-
-        // Comparator c = Collections.reverseOrder();
-
-        Collections.sort(list,new customSort());
-
-
-
-        for (Integer i : list) {
-            System.out.println("String value:" + i);
-        }
+        people.forEach(p -> System.out.println(p.toString()));
 
     }
-
-}
-
-class customSort implements Comparator<Integer>{
-
-	@Override
-	public int compare(Integer o1, Integer o2) {
-		return o1>o2?-1:o2< o1?0:1;
-	}
 
 }
--- src/main/java/com/corejava/Person.java
+++ src/main/java/com/corejava/Person.java
@@ -1,27 +1,41 @@
 package com.corejava;
 
-public class Person{
+public class Person implements Comparable<Person> {
 
     private String name;
     private int age;
 
-    Person(String name,int age){
+    Person(String name, int age) {
         super();
         this.setName(name);
         this.setAge(age);
     }
 
-	public String getName() {
-		return name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        return this.age > p.age ? -1 : p.age < this.age ? 1 : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {age= " + this.age + "name=" + this.name + "}";
+
+    }
 
 }
