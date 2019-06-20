24b007a6b313976ee8c8acc152647402d71454d4
Sudeep-cv <basicsinjava@gmail.com>
2019-06-20 08:55:52 +0000

    finalize- System.g() garbage collector

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -4,10 +4,21 @@ package com.corejava;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+     public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        App obj = new App();
+        System.out.println(obj.hashCode());
+        obj = null;
+        // calling garbage collector
+        System.gc();
+        System.out.println("end of garbage collection");
+
+    }
+    @Override
+    protected void finalize()
+    {
+        System.out.println("finalize method called");
     }
 }
