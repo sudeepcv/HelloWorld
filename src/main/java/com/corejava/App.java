b172834e06af290594cb80a80441dd2e28334a1c
Sudeep-cv <basicsinjava@gmail.com>
2019-11-12 11:57:11 +0000

    synchronized

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -1,35 +1,42 @@
 package com.corejava;
 
+class Counter{
+    int count;
+
+    public  synchronized void add(){
+        count++;
+    }
+}
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+       Counter counter=new Counter();
 
         Runnable runnable = () -> {
-            for (int i = 0; i < 5; i++) {
-                try {
+            for (int i = 0; i <1000; i++) {
+       
                     System.out.println("Hi");
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                    counter.add();
+                    
+ 
             }
         };
         Thread t1=new Thread(runnable);
         Thread t2=new Thread(()-> {
 
-                try {
-                    for (int i = 0; i < 5; i++) {
+              
+                    for (int i = 0; i < 1000; i++) {
                         System.out.println("Hello Thread");
-                        Thread.sleep(1000);
+                        counter.add();
+                        
 
                     }
 
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        
             });
-            t1.setPriority(1);
-            t2.setPriority(10);
-            // priority values from 1 to 10
+
+
         t1.start();
         t2.start();
 
@@ -38,6 +45,8 @@ public class App {
             System.out.println(t1.isAlive());
             t1.join();
             t2.join();
+
+            System.out.println("Counter->"+counter.count);
 
             System.out.println(t1.getPriority());
             System.out.println(t2.getPriority());
