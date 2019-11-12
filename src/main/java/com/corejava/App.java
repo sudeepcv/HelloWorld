6807b306a80768258f0d5cc07985feb790ee05f9
Sudeep-cv <basicsinjava@gmail.com>
2019-11-12 10:53:49 +0000

    anonymous inner class

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -1,46 +1,46 @@
 package com.corejava;
 
-class PrintHi implements Runnable {
-    public void run() {
-        for (int i = 0; i < 5; i++) {            
-            try {
-                System.out.println("Hi");
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {               
-                e.printStackTrace();
-            }
-        }
-
-    }
-}
-
-class Print implements Runnable {
-
-    public void run() {
-
-        try {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Hello Thread");
-                Thread.sleep(1000);
-
-            }
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-}
-
 public class App {
     public static void main(String[] args) {
-        Print print = new Print();
-        PrintHi printHi=new PrintHi();
 
-        Thread thread1 = new Thread(print);
-        Thread thread2 = new Thread(printHi);
-        thread1.start();
-        thread2.start();
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        System.out.println("Hi");
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+
+        Runnable runnable2 = new Runnable() {
+
+            public void run() {
+
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        System.out.println("Hello Thread");
+                        Thread.sleep(1000);
+
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1=new Thread(runnable);
+        Thread t2=new Thread(runnable2);
+        t1.start();
+        t2.start();
+
 
     }
 }
