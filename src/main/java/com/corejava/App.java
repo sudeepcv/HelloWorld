0195d54eda47025cb9404e4436597cf481c04ec7
Sudeep-cv <basicsinjava@gmail.com>
2019-11-13 06:21:57 +0000

    BlockingQueue

--- src/main/java/com/corejava/App.java
+++ src/main/java/com/corejava/App.java
@@ -1,63 +1,63 @@
 package com.corejava;
 
-class Counter{
-    int count;
-
-    public  synchronized void add(){
-        count++;
-    }
-}
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
 
-       Counter counter=new Counter();
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("Thread has been Started");
+        Thread t1=new Thread(new Runnable(){
+           
+            public void run(){
+                try{
+                     this.producer();
+                }catch(InterruptedException e){}
+               
 
-        Runnable runnable = () -> {
-            for (int i = 0; i <1000; i++) {
-       
-                    System.out.println("Hi");
-                    counter.add();
-                    
- 
             }
-        };
-        Thread t1=new Thread(runnable);
-        Thread t2=new Thread(()-> {
+        });
 
-              
-                    for (int i = 0; i < 1000; i++) {
-                        System.out.println("Hello Thread");
-                        counter.add();
-                        
 
-                    }
+                Thread t2=new Thread(new Runnable(){
+            
+            public void run(){
+                try{
+                    consumer();
+                }catch(InterruptedException e){}                
 
-        
-            });
-
+            }
+        });
 
         t1.start();
         t2.start();
 
-        try {
+        t1.join();
+        t2.join();
 
-            System.out.println(t1.isAlive());
-            t1.join();
-            t2.join();
 
-            System.out.println("Counter->"+counter.count);
+    }
 
-            System.out.println(t1.getPriority());
-            System.out.println(t2.getPriority());
-            System.out.println(t1.isAlive());
-               System.out.println("threads completed");
+    private static void producer() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            queue.put(random.nextInt(100));
 
-        } catch (InterruptedException e) {            
-            e.printStackTrace();
         }
-        
-     
+    }
+
+    private  static void consumer() throws InterruptedException {
+        Random random =new Random();
+        while(true){
+            Thread.sleep(100);
+            if(random.nextInt(10)==0){
+                Integer value=queue.take();
+                System.out.println("Taken value is:"+value+" and the size of the queue is :"+queue.size());
+            }
+
+        }
 
     }
 }
